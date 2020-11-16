ed0aef075789f4edac3789183b4d3e346edd940f
Petri Leskinen <petri.leskinen@aalto.fi>
2020-11-16 21:09:32 +0200

    remove pickle files

---
+++ python/make_pickles.py
@@ -0,0 +1,43 @@
+import argparse
+import glob 
+import pickle
+from rdflib import Graph 
+import re
+
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('-l', '--limit', type=int, default=None, help='Limit the number of results, for faster testing')
+    
+    parser.add_argument('-i', '--files', type=str, default="ttl", nargs='+', help='input folder')
+    
+    opts = parser.parse_args()
+    
+    for f in opts.files:
+        try:
+            g = Graph()
+            print("Parsing file {}".format(f))
+            g.parse(f, format="turtle")
+            
+            print("Parsed {} triples from {}".format(len(g), f))
+
+            if len(g):
+                outfile = re.sub(r'\.ttl$', '.pickle', f)
+                pickle.dump(g, open( outfile, "wb" ) )
+                print("Exported pickle file {}".format(outfile))
+        
+        except Exception as e:
+
+            print("Failed to parse {}".format(f))
+            print("error: {}".format(e))
+
+"""
+reading:
+g=Graph()
+g += pickle.load(open( f, "rb" ))
+"""
+
+if __name__ == "__main__":
+    main()
